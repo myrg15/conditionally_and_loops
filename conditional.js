1101639bacc710c280f69a424da82b4b6951cec1
myrg15 <merishield@gmail.com>
2024-02-01 22:30:23 +0100

    [feat:variable name change in conditional]

--- conditional.js
+++ conditional.js
@@ -2,18 +2,18 @@
 
 var num1 = prompt("Enter first number:");
 var firstNumber = parseInt(num1); // converts string to integer
-console.log("Number entered:", num1); //num1 contains the number entered by the user
+console.log("Number entered:", firstNumber); //num1 contains the number entered by the user
 
 var num2 = prompt("Enter second number:");
 var secondNumber = parseInt(num2);
-console.log("Number entered: ", num2);
+console.log("Number entered: ", secondNumber);
 
-if (num1 == num2) {
+if (firstNumber == secondNumber) {
   alert("Are the same");
 } else {
-  if (num1 > num2) {
+  if (firstNumber > secondNumber) {
     alert("The first number is greater than the second number");
-  } else if (num2 > num1) {
+  } else if (secondNumber > firstNumber) {
     alert("The second number is greater than the first number");
   }
 }
