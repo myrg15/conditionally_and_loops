468081ec031c9c23be5a77f0824f4243639f0460
myrg15 <merishield@gmail.com>
2024-02-07 09:42:56 +0100

    [feat:shows whether a number is positive, negative or null]

---
+++ positiveNumber.js
@@ -0,0 +1,13 @@
+//enter one number by keyboard, will show you if is positive, negative or null
+var num = prompt("Enter number:");
+var numberEn = parseInt(num);
+
+if (numberEn >= 0) {
+  alert("Number is positive: ", numberEn);
+} else {
+  if (numberEn < 0) {
+    alert("Number is negative: ", numberEn);
+  } else if (numberEn == null) {
+    alert("Number is null");
+  }
+}
